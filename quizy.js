33552f3b80caa7466435bcf50eace35169f4f4e6
Kazuki Nakazawa <kazunaka.0077@gmail.com>
2021-07-21 14:36:23 +0900

    選択肢をクリックした時の動作を変更しました

--- quizy.js
+++ quizy.js
@@ -5,23 +5,21 @@ false1.addEventListener('click',() => {
     false1.classList.add('failed1');
 })
 
+const description2 = document.getElementById('result-description2');
+const result2 = document.getElementById('result2');
 const false2 = document.getElementById('false2');
 false2.addEventListener('click',() => {
     false2.classList.add('failed2');
+    result2.textContent = '不正解です！';
+    description2.textContent = '正解は「たかなわ」です！';
 })
 
+const description1 = document.getElementById('result-description1');
+const result1 = document.getElementById('result1');
 const truth = document.getElementById('true');
 truth.addEventListener('click',() => {
     truth.classList.add('succeed');
+    result1.textContent = '正解です！';
+    description1.textContent = '正解は「たかなわ」です！';
 })
 
-// const false1 = document.getElementById('false1');
-// const false2 = document.getElementById('false2');
-// if (false1 == true || false2 == true){
-//     false1.addEventListener('click',() => {
-//         false1.classList.add('failed1');
-//     });
-//     false2.addEventListener('click',() => {
-//         false2.classList.add('failed2');
-//     });
-// }
