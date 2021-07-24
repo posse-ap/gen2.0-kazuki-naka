49b2288c42355942eb54f80333430b325d9e1421
Kazuki Nakazawa <kazunaka.0077@gmail.com>
2021-07-24 19:59:27 +0900

    不正解の選択肢を選択したら正解の選択肢の背景と文字の色が変化するようにしました

--- quizy.js
+++ quizy.js
@@ -1,25 +1,32 @@
 'use strict';
 
+
+const incorrectAnswer = document.getElementsByClassName('incorrectAnswer')[0];
 const false1 = document.getElementById('false1');
 false1.addEventListener('click',() => {
-    false1.classList.add('failed1');
+    false1.classList.add('failed');
+    incorrectAnswer.textContent = '不正解です！';
+    description.textContent = '正解は「たかなわ」です！';
+    choice[2].classList.add('succeed');
 })
 
-const description2 = document.getElementById('result-description2');
-const result2 = document.getElementById('result2');
+const choice = document.getElementsByClassName('choice');
 const false2 = document.getElementById('false2');
+
 false2.addEventListener('click',() => {
-    false2.classList.add('failed2');
-    result2.textContent = '不正解です！';
-    description2.textContent = '正解は「たかなわ」です！';
+    false2.classList.add('failed');
+    incorrectAnswer.textContent = '不正解です！';
+    description.textContent = '正解は「たかなわ」です！';
+    choice[2].classList.add('succeed');
 })
 
-const description1 = document.getElementById('result-description1');
+
+const description = document.getElementsByClassName('answerDescription')[0];
 const result1 = document.getElementById('result1');
 const truth = document.getElementById('true');
-truth.addEventListener('click',() => {
+truth.addEventListener('click',() =>{
     truth.classList.add('succeed');
     result1.textContent = '正解です！';
-    description1.textContent = '正解は「たかなわ」です！';
+    description.textContent = '正解は「たかなわ」です！';
 })
 
