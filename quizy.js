a6e81fdda431da1ac0044b662710fc739a143507
Kazuki Nakazawa <kazunaka.0077@gmail.com>
2021-07-25 18:09:51 +0900

    一問目が完成しました

--- quizy.js
+++ quizy.js
@@ -1,16 +1,19 @@
 'use strict';
 
-
+const choice = document.getElementsByClassName('choice');
 const incorrectAnswer = document.getElementsByClassName('incorrectAnswer')[0];
 const false1 = document.getElementById('false1');
+
 false1.addEventListener('click',() => {
     false1.classList.add('failed');
     incorrectAnswer.textContent = '不正解です！';
     description.textContent = '正解は「たかなわ」です！';
     choice[2].classList.add('succeed');
+    false2.classList.add('target');
+    truth.classList.add('target');
 })
 
-const choice = document.getElementsByClassName('choice');
+
 const false2 = document.getElementById('false2');
 
 false2.addEventListener('click',() => {
@@ -18,15 +21,20 @@ false2.addEventListener('click',() => {
     incorrectAnswer.textContent = '不正解です！';
     description.textContent = '正解は「たかなわ」です！';
     choice[2].classList.add('succeed');
+    false1.classList.add('target');
+    truth.classList.add('target');
 })
 
 
 const description = document.getElementsByClassName('answerDescription')[0];
 const result1 = document.getElementById('result1');
 const truth = document.getElementById('true');
+
 truth.addEventListener('click',() =>{
     truth.classList.add('succeed');
     result1.textContent = '正解です！';
     description.textContent = '正解は「たかなわ」です！';
+    false1.classList.add('target');
+    false2.classList.add('target');
 })
 
