2417ad1936dc6d8363bf6e0ed233e82fdf3d2912
Kazuki Nakazawa <kazunaka.0077@gmail.com>
2021-10-20 13:27:33 +0900

    ボタン押したときにローディング画面表示を実装

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const modal = document.getElementById('modal');
-document.getElementById('submit').addEventListener('click', () => {
+document.getElementById('top-submit').addEventListener('click', () => {
   modal.style.display = 'block';
 })
 
 document.getElementById('back-button').addEventListener('click', () => {
   modal.style.display = 'none';
+})
+
+document.getElementById('modal-submit').addEventListener('click', () => {
+  document.getElementById('spinner').style.display = 'block';
 })
